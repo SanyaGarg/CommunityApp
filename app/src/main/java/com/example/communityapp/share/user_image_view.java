d1ebb38c09b3be0620a158f725e9c3d20637b6db
Sanya <gargsanya828@gmail.com>
2020-06-25 12:39:18 +0530

    first commit

---
+++ app/src/main/java/com/example/communityapp/share/user_image_view.java
@@ -0,0 +1,14 @@
+package com.example.communityapp.share;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class user_image_view extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_image_view);
+    }
+}
---
+++ app/src/main/java/com/example/communityapp/share/user_video.java
@@ -0,0 +1,14 @@
+package com.example.communityapp.share;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class user_video extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_video);
+    }
+}
